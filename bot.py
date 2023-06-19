e3b327aa9c467af177ea8e49a675a74ae6de768f
glenren <glenren@student.ubc.ca>
2023-06-18 18:19:04 -0700

    edited comments

--- bot.py
+++ bot.py
@@ -1,17 +1,29 @@
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
 import random
 import json
 
+# sets the prefix (e.g. if prefix = "!", then the help command is "!help")
+prefix = "!"
+# set intents (ver 1.5)
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
+# so I don't leak my token to github...
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
 
 
+# runs the bot
 def run_bot():
-    bot.run('MTExOTQwMDU1NTMwMzk0MDEyMA.GYNOsL.1hgZ-IwQ6UJNTjpfrUJi5rYarqm287sGoZEZQI')
+    bot.run(TOKEN)
 
 
+# command !make:
+# - create your own question to add to the bot's question list
 @bot.command()
 async def make(ctx):
     with open("archive.json") as f:
@@ -38,6 +50,8 @@ async def make(ctx):
     await ctx.send("Your question has been added.")
 
 
+# command !quiz:
+# take a quiz of 5 random questions pulled from the bot's question list, and get given a score
 @bot.command()
 async def quiz(ctx):
     with open("archive.json") as f:
@@ -46,9 +60,9 @@ async def quiz(ctx):
     x = 1
     no_dupes = []
     while x < 6 and x < len(questions) and len(no_dupes) < len(questions):
-        rand = random.randint(0, len(questions)-1)
+        rand = random.randint(0, len(questions) - 1)
         while rand in no_dupes:
-            rand = random.randint(0, len(questions)-1)
+            rand = random.randint(0, len(questions) - 1)
         no_dupes.append(rand)
         await ctx.send("**Question " + str(x) + ":** " + questions[rand][0])
 
@@ -65,5 +79,5 @@ async def quiz(ctx):
         x += 1
     await ctx.send("That's the end of the quiz!")
     score_calc = (score / (x - 1)) * 100
-    await ctx.send("Your score: **" + str(score_calc) + "%** (" + str(score) + " out of " + str((x-1)) + ")")
+    await ctx.send("Your score: **" + str(score_calc) + "%** (" + str(score) + " out of " + str((x - 1)) + ")")
     await ctx.send("Thank you for participating! Feel free to add questions of your own by typing **!make**.")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import bot
 
+# calls the run_bot() function in bot.py
 if __name__ == "__main__":
     bot.run_bot()
